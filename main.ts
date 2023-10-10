106664998294cccf054620ccf2ef8e7c3d7af386
maskelog <blitzmaske@gmail.com>
2023-10-11 02:08:58 +0900

    옵시디언 폴더 동기화

--- main.ts
+++ main.ts
@@ -1,134 +1,106 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, PluginSettingTab, Setting, TFile, App, FileSystemAdapter, Vault } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FileSyncPluginSettings {
+    sourceFolder: string;
+    destFolder: string;
+    syncInterval: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FileSyncPluginSettings = {
+    sourceFolder: '',
+    destFolder: '',
+    syncInterval: 10
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FileSyncPlugin extends Plugin {
+    settings: FileSyncPluginSettings;
+    syncInterval: any;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        this.addSettingTab(new FileSyncSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.syncInterval = window.setInterval(this.syncFiles.bind(this), this.settings.syncInterval * 1000);
+    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    async saveSettings() {
+        await this.saveData(this.settings);
+        window.clearInterval(this.syncInterval);
+        this.syncInterval = window.setInterval(this.syncFiles.bind(this), this.settings.syncInterval * 1000);
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async syncFiles() {
+        const vault: Vault = this.app.vault;
+        const adapter: FileSystemAdapter = (vault.adapter as FileSystemAdapter);
+        const sourceFolderPath = this.settings.sourceFolder;
+        const destFolderPath = this.settings.destFolder;
+    
+        const allFiles: TFile[] = vault.getFiles().filter(file => file.path.startsWith(sourceFolderPath));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        for (const file of allFiles) {
+            const content = await vault.read(file);
+            const destFilePath = destFolderPath + '/' + file.path.replace(sourceFolderPath, '');
+            await adapter.write(destFilePath, content);
+        }
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class FileSyncSettingTab extends PluginSettingTab {
+    plugin: FileSyncPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: FileSyncPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        let { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
+        containerEl.createEl('h2', { text: 'File Sync Settings' });
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        new Setting(containerEl)
+            .setName('Source Folder')
+            .setDesc('The source folder in Obsidian')
+            .addText(text => text
+                .setPlaceholder('Enter source folder path')
+                .setValue(this.plugin.settings.sourceFolder)
+                .onChange(async (value) => {
+                    this.plugin.settings.sourceFolder = value;
+                }));
 
-	display(): void {
-		const {containerEl} = this;
+        new Setting(containerEl)
+            .setName('Destination Folder')
+            .setDesc('The destination folder for the files')
+            .addText(text => text
+                .setPlaceholder('Enter destination folder path')
+                .setValue(this.plugin.settings.destFolder)
+                .onChange(async (value) => {
+                    this.plugin.settings.destFolder = value;
+                }));
 
-		containerEl.empty();
+        new Setting(containerEl)
+            .setName('Sync Interval')
+            .setDesc('How often you want the files to be synced (in seconds)')
+            .addText(text => text
+                .setPlaceholder('Enter the sync interval')
+                .setValue(this.plugin.settings.syncInterval.toString())
+                .onChange(async (value) => {
+                    this.plugin.settings.syncInterval = parseInt(value);
+                }));
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Save Settings')
+            .addButton(button => {
+                button.setButtonText('Save').onClick(async () => {
+                    await this.plugin.saveSettings();
+                    new Notice('Settings saved!');
+                });
+            });
+    }
 }
